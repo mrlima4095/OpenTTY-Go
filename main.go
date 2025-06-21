5113d01307b23d89e8d6f8a6a508686a72d3f035
Lima <felipebr4095@gmail.com>
2025-06-21 07:58:13 -0300

    update main.go

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ func (otty *OpenTTY) x11(command string) {
 }
 
 func getCommand(input string) string { parts := strings.Fields(input); if len(parts) == 0 { return ""; } return parts[0]; }
-func getArgument(input string) string { parts := strings.Fields(input) if len(parts) <= 1 { return ""; }; return strings.Join(parts[1:], " "); }
+func getArgument(input string) string { parts := strings.Fields(input); if len(parts) <= 1 { return ""; } return strings.Join(parts[1:], " "); }
 
 func main() {
 	otty := NewOpenTTY()
